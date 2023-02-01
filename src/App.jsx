b8c59626b0b053e284e965046c71fead95e1f48e
OjashKush <ojashkush@gmail.com>
2023-02-02 01:03:59 +0530

    shows basic info from an api

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,33 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "./axios.jsx";
 
+
+const UserDetails = ({ post }) => {
+  const [expanded, setExpanded] = useState(false);
+  const { id, username, address, name, email, phone, website, company } = post;
+
+  return (
+    <div key={id} className="card">
+      <h2>{name.slice(0, 15).toUpperCase()}</h2>
+      <p>UserName: {username}</p>
+      <p>City: {address.city}</p>
+      {expanded && (
+        <div className="more">
+          <p>Email: {email}</p>
+          <p>Phone: {phone}</p>
+          <p>Website: {website}</p>
+          <p>Company: {company.name}</p>
+          <p>Firm's Moto : {company.catchPhrase}</p>
+          <p>Firm Details : {company.bs}</p>
+        </div>
+      )}
+      <button onClick={() => setExpanded(!expanded)}>
+        {expanded ? "Hide Details" : "View Details"}
+      </button>
+    </div>
+  );
+};
+
 const App = () => {
   const [myData, setMyData] = useState([]);
   const [isError, setIsError] = useState("");
@@ -9,7 +36,7 @@ const App = () => {
   // using Async Await
   const getMyPostData = async () => {
     try {
-      const res = await axios.get("/posts");
+      const res = await axios.get("/users");
       setMyData(res.data);
     } catch (error) {
       setIsError(error.message);
@@ -23,19 +50,13 @@ const App = () => {
 
   return (
     <>
-      <h1>Axios Tutorial</h1>
+      <h1>Basic Information</h1>
       {isError !== "" && <h2>{isError}</h2>}
 
       <div className="grid">
-        {myData.slice(0, 9).map((post) => {
-          const { body, id, title } = post;
-          return (
-            <div key={id} className="card">
-              <h2>{title.slice(0, 15).toUpperCase()}</h2>
-              <p>{body.slice(0, 100)}</p>
-            </div>
-          );
-        })}
+        {myData.slice(0, 5).map((post) => (
+          <UserDetails key={post.id} post={post} />
+        ))}
       </div>
     </>
   );
--- src/axios.jsx
+++ src/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // we need to pass the baseURL as an object
 const API = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com/users",
+  baseURL: "https://jsonplaceholder.typicode.com",
 });
 
 export default API;
